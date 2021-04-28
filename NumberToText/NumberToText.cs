2a6b96bd911ad8934bb19728d8afe3fc37384433
Вячеслав Островский <slslava007@gmail.com>
2021-04-28 16:57:46 +0300

    Add project files.

---
+++ NumberToText/NumberToText.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToText
+{
+    class NumberToText
+    {
+        private Dictionary<int, string> dictionary;
+        private int number;
+
+        public NumberToText(int number)
+        {
+            this.number = number;
+        }
+        public void convertNumber()
+        {
+            dictionary = File.ReadLines("Dictionary.txt").Select(line => line.Split('\t')).Where(arr => arr.Length == 2).ToDictionary(arr => Convert.ToInt32(arr[0]), arr => arr[1]);
+
+            foreach (var dict in dictionary)
+            {
+                if (dict.Key == number)
+                    Console.WriteLine(dict.Key + " - " + dict.Value);
+            }
+
+        }
+    }
+}
---
+++ NumberToText/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.WriteLine("Введите целое число которое нужно преобразовать в прописной вариант: ");
+            Console.Write(" > ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            NumberToText numberInText = new NumberToText(number);
+            numberInText.convertNumber();
+
+            Console.WriteLine("Программа завершена");
+            Console.ReadLine();
+        }
+    }
+}
